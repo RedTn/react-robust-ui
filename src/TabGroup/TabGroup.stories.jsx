0080846450a090ddb3d60b4dba8dcb31a78a09aa
Redmond Tran <redmondtran@builddirect.com>
2019-04-03 09:37:31 -0700

    feat: upgrade to storybook v5

--- src/TabGroup/TabGroup.stories.jsx
+++ src/TabGroup/TabGroup.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import TabGroup from './TabGroup.jsx';
 import { action } from '@storybook/addon-actions';
-import centered from '@storybook/addon-centered';
+import centered from '@storybook/addon-centered/react';
 import { withKnobs } from '@storybook/addon-knobs';
 
 storiesOf('TabGroup', module)
